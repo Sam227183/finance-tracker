812b92625992ae8b107424674aab7c0e821e60ea
Hansaem Kim <dalbyul9062@gmail.com>
2026-04-16 16:47:36 -0600

    Set associations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+  
   def self.market_price(ticker)
     ticker = ticker.upcase.strip
     query = BasicYahooFinance::Query.new
