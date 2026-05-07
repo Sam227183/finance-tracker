89e5d528d37e524f96c790a7d73ef0e11cad52b6
Hansaem Kim <dalbyul9062@gmail.com>
2026-05-06 19:24:01 -0600

    Add my friends view with listing of followed friends

--- app/controllers/friendships_controller.rb
+++
@@ -1,4 +0,0 @@
-class FriendshipsController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create, :destroy]
+  resources :user_stocks, only: [ :create, :destroy ]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   root "home#index"
   get "my_portfolio", to: "users#my_portfolio"
   post "search_stock", to: "stocks#search"
+  get "my_friends", to: "users#my_friends"
 end
