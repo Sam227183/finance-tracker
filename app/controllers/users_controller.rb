65ccbbf0fddfc785da17e57346232431ace03827
Hansaem Kim <dalbyul9062@gmail.com>
2026-05-26 17:33:03 -0600

    Added search on friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = User
+                  .where("lower(first_name) = ? or lower(last_name) = ? or lower(email) = ?", params[:friend].downcase, params[:friend].downcase, params[:friend].downcase)
+                  .first
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to my_friends_path }
+      end
+    else
+      flash[:alert] = "Please enter a friend name or email to search"
+      redirect_to my_friends_path
+    end
+  end
 end
